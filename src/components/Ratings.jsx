a2c1aaad467dce5e56b5a2f0c338440b55141536
Mpho Cindi <cindimpho3@gmil.com>
2026-03-25 20:31:04 +0200

    adding useState hook

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 const Ratings = () => {
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
     const stars = Array.from({length: 5}, (_, i) => i + 1)
   return ( 
   <div className="rating-container">
     <h2>Rate your experience</h2>
     <div className="stars">
         {stars.map((star) => (
-            <span key={star} className="star">
+            <span 
+             onClick={ () => setRating(star)} 
+             onMouseEnter={ () => setHover(star)}
+             onMouseLeave={ () => setHover(0)}
+             key={star} className="star">
                 {'\u2605'}
             </span>
         ))}
